174dec28cc4af1e158eb6f15b81be5a7c3e78d74
Shaheed08 <daleel5880@gmail.com>
2022-11-26 13:22:02 -0800

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python3
 """
-
-This module is composed by a function that divides the numbers of a matrix
-
+    divides all elements of a matrix
 """
 
 
 def matrix_divided(matrix, div):
+    """
+        forms a new matrix after all elements are divided
+    """
 
-    if not type(div) in (int, float):
+    new_matrix = []
+    if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
-
-    if not matrix or not isinstance(matrix, list):
-        raise TypeError(msg_type)
-
-    len_e = 0
-    msg_size = "Each row of the matrix must have the same size"
-
-    for elems in matrix:
-        if not elems or not isinstance(elems, list):
-            raise TypeError(msg_type)
-
-        if len_e != 0 and len(elems) != len_e:
-            raise TypeError(msg_size)
-
-        for num in elems:
-            if not type(num) in (int, float):
-                raise TypeError(msg_type)
-
-        len_e = len(elems)
-
-    m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
-    return (m)
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        transpose = []
+        for elem in row:
+            if type(elem) not in [int, float]:
+                raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+            elem = round(elem / div, 2)
+            transpose.append(elem)
+        new_matrix.append(transpose)
+    return new_matrix
